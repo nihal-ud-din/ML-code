52d06ce15ec64628c7d6bba3a7153b243bc95715
nihal-ud-din <nihaluddin707@gmail.com>
2024-10-03 23:48:57 +0530

    web app for model

--- house-price-prediction/main.py
+++ house-price-prediction/main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
-
-# Press ⌃F5 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask, render_template, request
+import pandas as pd
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+app = Flask(__name__)
+data = pd.read_csv('cleaned_data.csv')
+@app.route('/')
+def index():
 
+    locations = sorted(data['location'].unique())
+    return render_template('index.html', locations=locations)
 
-# Press the green button in the gutter to run the script.
+@app.route('/predict', methods=['POST'])
+def predict():
+    return ""
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True, port=5001)
