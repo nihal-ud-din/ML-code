0e4ac69e2a46bbfac7a8a6fb8b5cbdf6691eb058
nihal-ud-din <nihaluddin707@gmail.com>
2024-10-04 01:57:46 +0530

    web code updated

--- house-price-prediction/main.py
+++ house-price-prediction/main.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request
 import pandas as pd
+import pickle
+import numpy as np
 
 
 app = Flask(__name__)
 data = pd.read_csv('cleaned_data.csv')
+pipe = pickle.load(open("RidgeModel.pkl",'rb'))
 @app.route('/')
 def index():
 
@@ -12,7 +15,15 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    return ""
+
+    location = request.form.get('location')
+    bhk = request.form.get('bhk')
+    bath = request.form.get('bath')
+    sqft = request.form.get('total_sqft')
+    print(location, bhk, bath, sqft)
+    input = pd.DataFrame([[location,sqft,bath,bhk]], column=['location','totla_sqft', 'bath', 'bhk'])
+    prediction = pipe.predict(input)[0] *1e5
+    return str(np.round(prediction,2))
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
